1c641c1a4f00a4cb0b0f7b4d82e844fc48224425
Santoso Wijaya <santoso.wijaya@gmail.com>
2013-04-19 17:52:38 -0700

    Add mrunit dependency and test skeleton.

--- src/test/java/com/swijaya/hostess/HostessTest.java
+++ src/test/java/com/swijaya/hostess/HostessTest.java
@@ -1,4 +1,37 @@
 package com.swijaya.hostess;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
 public class HostessTest {
+
+    private MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+    private ReduceDriver<Text, IntWritable, Text, Text> reduceDriver;
+    private MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, Text> mapReduceDriver;
+
+    @Before
+    public void setUp() {
+        Hostess.Map mapper = new Hostess.Map();
+        Hostess.Reduce reducer = new Hostess.Reduce();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+    }
+
+    @Test
+    public void testMapper() {
+
+    }
+
+    @Test
+    public void testReducer() {
+
+    }
+
 }
